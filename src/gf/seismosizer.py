9637f3cc5bd45c81d24efcfada9f6eb8cfaf8d26
Wanderelch <malte.metz@posteo.de>
2020-03-24 16:16:18 +0100

    okada: change in delta_slip calculation
    calculation times starts from zero instead of t_min with constant dt to prevent results from bias
    due to irregular time steps.
    gradient is replaced by diff as the gradient approximates the slip function as a curve and
    hence induces slip rates even before the rupture has reached the boundary element.

--- src/gf/seismosizer.py
+++ src/gf/seismosizer.py
@@ -2400,6 +2400,7 @@ class PseudoDynamicRupture(SourceWithDerivedMagnitude):
 
         if not isinstance(
                 nucleation_x, num.ndarray) and nucleation_x is not None:
+
             nucleation_x = num.array([nucleation_x])
 
         self.nucleation_x__ = nucleation_x
