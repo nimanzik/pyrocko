5aae1c5a5d3ded42c42b310fb7e8b645bbd73127
miili <isken@gfz-potsdam.de>
2020-12-15 18:22:26 +0100

    squirrel.io: adding segmented mseed files

--- src/squirrel/io/backends/mseed.py
+++ src/squirrel/io/backends/mseed.py
@@ -8,6 +8,8 @@ from __future__ import absolute_import, print_function
 from pyrocko.io.io_common import get_stats, touch  # noqa
 from ... import model
 
+SEGMENT_NRECORDS = 512
+
 
 def provided_formats():
     return ['mseed']
@@ -24,28 +26,41 @@ def detect(first512):
 
 def iload(format, file_path, segment, content):
     assert format == 'mseed'
-
     from pyrocko import mseed
 
     load_data = 'waveform' in content
+    load_segment = True
 
-    for itr, tr in enumerate(mseed.iload(file_path, load_data=load_data)):
+    if segment is None:
+        segment = 0
+        load_segment = False
 
-        nsamples = int(round((tr.tmax - tr.tmin) / tr.deltat)) + 1
+    ntr = 1  # Accessing the while loop
+    while ntr:
+        ntr = 0
+        for itr, tr in enumerate(mseed.iload(
+                file_path, load_data=load_data,
+                segment=segment, segment_nrecords=SEGMENT_NRECORDS)):
+            ntr += 1
 
-        nut = model.make_waveform_nut(
-            file_segment=0,
-            file_element=itr,
-            agency='',
-            network=tr.network,
-            station=tr.station,
-            location=tr.location,
-            channel=tr.channel,
-            tmin=tr.tmin,
-            tmax=tr.tmin + tr.deltat * nsamples,
-            deltat=tr.deltat)
+            nsamples = int(round((tr.tmax - tr.tmin) / tr.deltat)) + 1
+            nut = model.make_waveform_nut(
+                file_segment=segment,
+                file_element=itr,
+                agency='',
+                network=tr.network,
+                station=tr.station,
+                location=tr.location,
+                channel=tr.channel,
+                tmin=tr.tmin,
+                tmax=tr.tmin + tr.deltat * nsamples,
+                deltat=tr.deltat)
 
-        if 'waveform' in content:
-            nut.content = tr
+            if 'waveform' in content:
+                nut.content = tr
 
-        yield nut
+            yield nut
+
+        if load_segment:
+            return
+        segment += 1
