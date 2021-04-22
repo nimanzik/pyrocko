ea698ff3ef520aa57087c2149958bf416e026e8a
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2021-04-22 10:34:15 +0200

    polarization snuffling: adaptive filter frequency range

--- src/gui/snufflings/polarization.py
+++ src/gui/snufflings/polarization.py
@@ -144,6 +144,23 @@ Scale*.
         self.nsl_to_amax = {}
         self.last_rotate_to_rt = False
 
+    def panel_visibility_changed(self, visible):
+        viewer = self.get_viewer()
+        if visible:
+            viewer.pile_has_changed_signal.connect(self.adjust_controls)
+            self.adjust_controls()
+
+        else:
+            viewer.pile_has_changed_signal.disconnect(self.adjust_controls)
+
+    def adjust_controls(self):
+        viewer = self.get_viewer()
+        dtmin, dtmax = viewer.content_deltat_range()
+        maxfreq = 0.5/dtmin
+        minfreq = (0.5/dtmax)*0.001
+        self.set_parameter_range('lowpass', minfreq, maxfreq)
+        self.set_parameter_range('highpass', minfreq, maxfreq)
+
     def get_selected(self):
         markers = [
             marker for marker in self.get_selected_markers()
